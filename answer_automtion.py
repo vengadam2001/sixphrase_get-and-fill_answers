ad0553078f5f6e8c5ff1633943901823ea5bddc5
stv <vengadam2001@gmail.com>
2022-01-03 15:34:09 +0530

    sixphrase automation project

---
+++ answer_automtion.py
@@ -0,0 +1,52 @@
+from    selenium                           import webdriver
+from    selenium.webdriver.chrome.options  import Options
+from    scrapy.selector                    import Selector 
+# import time
+# from bs4 import BeautifulSoup
+# from lxml import etree
+# import pyautogui
+# from time import sleep
+
+driver = webdriver.Chrome('./chromedriver.exe')
+driver.get("http://sixphrase.com/users/sign_in")
+print(driver.title)
+user = driver.find_element_by_id("user_email")
+passwd = driver.find_element_by_id("user_password")
+# username = input("\n\n\n\nenter baliaadu account, format: name.UniqueId: ")
+username = "karan.e0119039"
+user.send_keys(username+"@sret.edu.in")
+passwd.send_keys("myslate")
+driver.find_element_by_name("commit").click()
+driver.get(input("\n\n\n\n enter the answer source link full http :"))
+driver.find_element_by_xpath("/html/body/header/div/div/div/div/ul/li[2]/a").click()
+source = driver.page_source
+source = Selector(text = source)
+i=1
+qote="'"
+qotes='"'
+flag=False
+while (i<=10):
+    divs = source.xpath('//div[@id="code-editor'+str(i)+'"]/div[2]/div/div[3]/div')
+    string =''
+    for div in divs:
+        for z in div.xpath('./div/span'):
+            # print(z.xpath("text()").get())
+            if  z.xpath("text()").get()==qotes:
+                flag=not(flag)
+                print("hello ggwp \n")
+            if flag == False:      
+                string += z.xpath("text()").get()+" "
+            else:
+                string += z.xpath("text()").get()
+        string+='\n'
+    print(i)
+    f= open(str(i)+".txt",'w')
+    f.write(string)
+    f.close()
+    i+=1
+# soup = BeautifulSoup(driver.page_source, "html.parser")
+# dom = etree.HTML(str(soup))
+# print(dom.xpath('//div[@id="code-editor1"]/div[2]/div/div[3]/div')[0].text)
+# soup = BeautifulSoup(source, 'html.parser')
+# print(soup.prettify())
+driver.close()
---
+++ automation.py
@@ -0,0 +1,49 @@
+from    selenium                           import webdriver
+from    selenium.webdriver.chrome.options  import Options
+import time
+import pyautogui
+from time import sleep
+chrome_options = Options()
+chrome_options.add_argument("--window-size=1200,1080")
+driver = webdriver.Chrome('./chromedriver.exe',options=chrome_options)
+driver.get("http://sixphrase.com/users/sign_in")
+print(driver.title)
+user = driver.find_element_by_id("user_email")
+passwd = driver.find_element_by_id("user_password")
+user.send_keys("thiruvengadam.e0119023@sret.edu.in")
+passwd.send_keys("myslate")
+driver.find_element_by_name("commit").click()
+driver.get("http://sixphrase.com/course/5fb95ef719ab0a0f150e856c")
+# print(input("enter something when you complete selecting the test"))
+print(input("\n\n\n\nOpen the test and enter something when cursor is on the text box\n\n\n\n"),pyautogui.position())
+x,y=pyautogui.position()
+save = driver.find_element_by_id("save")
+submit = driver.find_element_by_id("submit_code")
+next = driver.find_element_by_id("next")
+for i in range(0,10):
+    pyautogui.click(x,y)
+    f=open(str(i+1)+".txt",'r')    
+    string = f.read()
+    print("")
+    pyautogui.typewrite(string)
+    cnt=0
+    for s in string:
+        if s=='{':
+            cnt+=1
+    # pyautogui.keyDown("ctrl")
+    # pyautogui.keyDown("shift")
+    # pyautogui.press("up")
+    # pyautogui.keyUp("ctrl")
+    # pyautogui.keyUp("shift")
+    # pyautogui.press("del")
+    print("")
+    sleep(10)
+    save.click()
+    submit.click()
+    sleep(20)
+    if i!=9:
+        next.click()
+    else:
+        print("\n\n\n\n\t\t\tcompleted "+input("absd")+"\n\n\n\n")
+sleep(10)
+driver.close()
